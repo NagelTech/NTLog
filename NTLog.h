8e9c93519a8728134069be7cf7a41ae62daca15c
Ethan Nagel <eanagel@gmail.com>
2012-08-16 01:50:19 -0700

    Simple NTLog base code

---
+++ NTLog.h
@@ -0,0 +1,29 @@
+//
+//  NTLog.h
+//
+//
+
+#import <Foundation/Foundation.h>
+
+
+typedef enum
+{
+    NTLogEntryTypeDebug   = 1,
+    NTLogEntryTypeLog     = 2,
+    NTLogEntryTypeWarn    = 4,
+    NTLogEntryTypeError   = 8,
+    NTLogEntryTypeFatal   = 16,
+} NTLogEntryType;
+
+
+
+#define NTLogDebug(...)         NTLog_Log(@__FILE__, __LINE__, NTLogEntryTypeDebug, __VA_ARGS__)
+#define NTLog(...)              NTLog_Log(@__FILE__, __LINE__, NTLogEntryTypeLog, __VA_ARGS__)
+#define NTLogWarn(...)          NTLog_Log(@__FILE__, __LINE__, NTLogEntryTypeWarn, __VA_ARGS__)
+#define NTLogError(...)         NTLog_Log(@__FILE__, __LINE__, NTLogEntryTypeError, __VA_ARGS__)
+#define NTLogFatal(...)         NTLog_Log(@__FILE__, __LINE__, NTLogEntryTypeFatal, __VA_ARGS__)
+
+
+
+void NTLog_Log(NSString *filename, int lineNum, NTLogEntryType logEntryType, NSString *format, ...);
+
